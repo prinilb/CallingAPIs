d186421f6f4fe98688bb8440178145f732cd2582
prinilb <prinilb@dunwoody.edu>
2022-05-10 15:29:07 -0500

    Add project files.

---
+++ DemoLibrary/ComicModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class ComicModel
+    {
+        public int num { get; set; }
+        public string img { get; set; }
+    }
+}
